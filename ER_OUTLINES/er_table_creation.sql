6e4f38b17e70787868d73341a66f3213d0b81193
EduardoNeville <eduardo.nevillecastro@epfl.ch>
2023-04-06 11:08:08 +0200

    Updated sql added todo and changed proj_business name

--- ER_OUTLINES/er_table_creation.sql
+++ ER_OUTLINES/er_table_creation.sql
@@ -1,3 +1,5 @@
+-- Users table
+
 CREATE TABLE USERS (
         Uid INT NOT NULL AUTO_INCREMENT,
         Name VARCHAR(20) NOT NULL,
@@ -5,21 +7,28 @@ CREATE TABLE USERS (
         average_stars FLOAT,
         review_count INT NOT NULL,
         fans INT NOT NULL, -- TBD if this is the correct way maybe we find way to tell if fan
-        elite BOOLEAN NOT NULL,
-        compliment_cool INT NOT NULL,
-        compliment_funny INT NOT NULL,
-        compliment_hot INT NOT NULL,
-        compliment_list INT NOT NULL,
-        compliment_more INT NOT NULL,
-        compliment_photos INT NOT NULL,
-        compliment_plain INT NOT NULL,
-        compliment_profile INT NOT NULL,
-        compliment_writer INT NOT NULL,
+
+        compliment_cool INT,
+        compliment_funny INT,
+        compliment_hot INT, 
+        compliment_list INT,
+        compliment_more INT,
+        compliment_photos INT,
+        compliment_plain INT, 
+        compliment_profile INT,
+        compliment_writer INT,
         -- not sure about this part
-        cool INT NOT NULL,
-        funny INT NOT NULL,
-        useful INT NOT NULL,
+        cool INT,
+        funny INT, 
+        useful INT ,
         PRIMARY KEY (Uid)
+);
+
+CREATE TABLE ELITE(
+        Uid INT NOT NULL,
+        Year INT NOT NULL,
+        PRIMARY KEY (Uid, Year),
+        FOREIGN KEY (Uid) REFERENCES USERS(Uid)
 );
 
 CREATE TABLE REVIEWS (
@@ -58,6 +67,10 @@ CREATE TABLE BUSINESSES (
         Foreign Key (Attributes) REFERENCES ATTRIBUTES(Attributes)
 );
 
+-- Categories of a business
+
+-- Business 'has' a category
+
 CREATE TABLE CATEGORIES (
         Bid INT NOT NULL,
         Category VARCHAR(100) NOT NULL,
@@ -65,14 +78,13 @@ CREATE TABLE CATEGORIES (
         FOREIGN KEY (Bid) REFERENCES BUSINESSES(Bid)
 );
 
-
-
-CREATE TABLE SCHEDULE (
+CREATE TABLE SCHEDULES (
+        Sid INT NOT NULL AUTO_INCREMENT,
         Bid INT NOT NULL,
-        Day DATE,
-        Open_time TIME,
-        Close_time TIME,
-        PRIMARY KEY (Bid, Day),
+        Day VARCHAR(10),
+        Open_time VARCHAR(10),
+        Close_time VARCHAR(20),
+        PRIMARY KEY (Bid, Sid),
         FOREIGN KEY (Bid) REFERENCES BUSINESSES(Bid)
 );
 
@@ -80,8 +92,9 @@ CREATE TABLE TIPS (
         Tid INT NOT NULL AUTO_INCREMENT,
         Uid INT NOT NULL,
         Bid INT NOT NULL,
-        Text VARCHAR(1000) NOT NULL,
-        Date DATE NOT NULL,
+        Text VARCHAR(1000),
+        Date DATE, 
+        Complement_count INT, 
         PRIMARY KEY (Tid),
         FOREIGN KEY (Uid) REFERENCES USERS(Uid),
         FOREIGN KEY (Bid) REFERENCES BUSINESSES(Bid)
@@ -90,24 +103,81 @@ CREATE TABLE TIPS (
 CREATE TABLE FRIENDS (
         Uid INT NOT NULL,
         Friend_uid INT NOT NULL,
+        PRIMARY KEY (Uid, Friend_uid)
         FOREIGN KEY (Uid) REFERENCES USERS(Uid),
         FOREIGN KEY (Friend_uid) REFERENCES USERS(Uid)
 );
 
 
-CREATE TABLE ATTRIBUTES (
+-- Attributes of the business
+-- Business has a 'ISA' relationship with them 
+
+CREATE TABLE BUSINESS_PARKING(
         Bid INT NOT NULL,
-        Attributes VARCHAR(1000) NOT NULL,
+        garage BOOLEAN,
+        street BOOLEAN,
+        validated BOOLEAN,
+        lot BOOLEAN,
+        valet BOOLEAN,
         PRIMARY KEY (Bid),
         FOREIGN KEY (Bid) REFERENCES BUSINESSES(Bid)
 );
 
-"""
-Check if this is really needed
-CREATE TABLE FANS (
-        Uid INT NOT NULL,
-        Fan_uid INT NOT NULL,
-        FOREIGN KEY (Uid) REFERENCES USERS(Uid),
-        FOREIGN KEY (Fan_uid) REFERENCES USERS(Uid)
+CREATE TABLE AMBIENCE(
+        Bid INT NOT NULL,
+        touristy BOOLEAN,
+        hipster BOOLEAN,
+        romantic BOOLEAN,
+        intimate BOOLEAN,
+        trendy BOOLEAN,
+        upscale BOOLEAN,
+        classy BOOLEAN,
+        casual BOOLEAN,
+        PRIMARY KEY (Bid),
+        FOREIGN KEY (Bid) REFERENCES BUSINESSES(Bid)
 );
-"""
+
+CREATE TABLE NOISELEVEL(
+        Bid INT NOT NULL,
+        noise_level VARCHAR(10),
+        PRIMARY KEY (Bid),
+        FOREIGN KEY (Bid) REFERENCES BUSINESSES(Bid)
+);
+
+CREATE TABLE GOODFORMEAL(
+        Bid INT NOT NULL,
+        dessert BOOLEAN,
+        latenight BOOLEAN,
+        lunch BOOLEAN,
+        dinner BOOLEAN,
+        brunch BOOLEAN,
+        breakfast BOOLEAN,
+        PRIMARY KEY (Bid),
+        FOREIGN KEY (Bid) REFERENCES BUSINESSES(Bid)
+); 
+
+CREATE TABLE MUSIC(
+        dj BOOLEAN,
+        background_music BOOLEAN,
+        no_music BOOLEAN,
+        jukebox BOOLEAN,
+        live BOOLEAN,
+        video BOOLEAN,
+        karaoke BOOLEAN,
+        PRIMARY KEY (Bid),
+        FOREIGN KEY (Bid) REFERENCES BUSINESSES(Bid)
+);
+
+CREATE TABLE DIETARY_RESTRICTIONS(
+        Bid INT NOT NULL,
+        dairy_free BOOLEAN,
+        gluten_free BOOLEAN,
+        vegan BOOLEAN,
+        kosher BOOLEAN,
+        halal BOOLEAN,
+        soy_free BOOLEAN,
+        vegetarian BOOLEAN,
+        PRIMARY KEY (Bid),
+        FOREIGN KEY (Bid) REFERENCES BUSINESSES(Bid)
+);
+
