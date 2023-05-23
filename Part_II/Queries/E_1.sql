9250aa50e852bfeffb69a781e9e273fa3397180e
EduardoNeville <eduardo.nevillecastro@epfl.ch>
2023-05-23 17:06:49 +0200

    reorganised the folders

---
+++ Part_II/Queries/E_1.sql
@@ -0,0 +1,11 @@
+
+--1. Find the businesses (name, review_count) in the city of 
+--'las vegas' that have 'valet' parking, 5 stars and are
+--open on Friday. Order by the business name in alphabetical 
+--order.
+
+
+
+
+
+
