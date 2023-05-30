dd7784070ba7368447642fcafd709a728d2a9c5b
paulblgr <61545982+paulblgr@users.noreply.github.com>
2023-05-30 23:14:41 +0200

    description E6,7,8 in report

--- Part_II/Queries/E_8.sql
+++ Part_II/Queries/E_8.sql
@@ -1,7 +1,8 @@
 --Find the minimum, maximum, mean, and median number of categories per business.
 --Return 4 columns (min_categories, max_categories, mean_categories, median_categories).
 
-SELECT min(ct) as min_categories, max(ct) as max_categories, AVG(ct) as mean_categories, MEDIAN(ct) as median_categories
+SELECT min(ct) as min_categories, max(ct) as max_categories, 
+AVG(ct) as mean_categories, MEDIAN(ct) as median_categories
 FROM 
     (SELECT COALESCE(BCs_ct.ct, 0) as ct
     FROM (BUSINESS
